2503c527dbe7b6cff43c3504c7c06ee03390c58e
William Califa <williamcalifa@MacBook-Pro-de-William.local>
2026-06-20 16:41:40 -0300

    Revert "Add Fras-le capacitação dashboard (EAD, Assistência Técnica, Eventos & Trade)"
    This reverts commit 216c0394007f90c7f62e54c45b99d3ec5488924c.

--- fras-le-capacitacao/client/src/lib/regions.js
+++
@@ -1,5 +0,0 @@
-export const UFS = [
-  "SP", "RS", "MG", "PR", "SC", "BA", "RJ", "GO", "PE", "ES",
-  "CE", "DF", "PA", "MT", "MS", "MA", "RN", "PB", "AL", "SE",
-  "RO", "AM", "AC", "AP", "RR", "TO", "PI",
-];
--- fras-le-capacitacao/server/repositories/index.js
+++
@@ -1,5 +0,0 @@
-import { createJsonRepository } from "./jsonRepository.js";
-
-export const eadRepository = createJsonRepository("ead.json");
-export const assistenciaRepository = createJsonRepository("assistencia-tecnica.json");
-export const eventosRepository = createJsonRepository("eventos-trade.json");
